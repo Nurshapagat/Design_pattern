a23876201588dba422a360ae8e292e56adf2e20b
Nurshapagat <maratovnashapi@gmail.com>
2025-05-16 10:12:08 +0600

    Implemented Main.java to initialize game and show window

---
+++ Main.java
@@ -0,0 +1,30 @@
+package src.main;
+
+import javax.swing.JFrame;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setTitle("Dungeon Survivor");
+		GamePanel gamePanel = new GamePanel();
+		frame.add(gamePanel);
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		GraphicsDevice gd = ge.getDefaultScreenDevice();
+		if (gd.isFullScreenSupported()) {
+			frame.setUndecorated(true);
+			frame.pack();
+			frame.setLocationRelativeTo(null);
+			gd.setFullScreenWindow(frame);
+		} else {
+			frame.pack();
+			frame.setLocationRelativeTo(null);
+			frame.setVisible(true);
+		}
+		gamePanel.startGameThread();
+	}
+}
